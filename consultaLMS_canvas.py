f638cc16128ed0c425a2187d595e42a2f245be76
unknown <tqaw19@gmail.com>
2018-09-25 19:32:03 -0500

    primer commit

---
+++ consultaLMS_canvas.py
@@ -0,0 +1,25 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://tecsup.instructure.com"
+# Canvas API key
+API_KEY = input("ingrese su token generado"+'\n')
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+
+class Curso:
+    def imprimir():
+        if API_KEY is None:
+            print("Su Token es incorrecto")
+        else:
+            course = canvas.get_course(3430) 
+            print("Nombre del curso:",course.name+'\n',
+                  "Fecha de inicio:",course.start_at+'\n',
+                  "Fecha fin:",course.end_at)
+
+            courses = user.get_courses()
+            print(courses)
+
+Curso.imprimir()
